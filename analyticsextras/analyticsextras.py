5a8f9dc98dc38ef9e7e3815b3017b3ff44318f77
uw-ray <uw.rayblaga90@gmail.com>
2015-07-23 17:56:54 -0400

    More work on recording student time, and a different system to send settings over to the JS code.

--- analyticsextras/analyticsextras.py
+++ analyticsextras/analyticsextras.py
@@ -54,6 +54,11 @@ class AnalyticsExtrasXBlock(XBlock):
         scope=Scope.user_state
     )
 
+    tick_period = Integer(
+        default=3000,
+        help="The time (in ms) between pings sent to the server (tied to sessions above)",
+        scope=Scope.content
+    )
 
 
     """
@@ -69,6 +74,15 @@ class AnalyticsExtrasXBlock(XBlock):
 
     """
 
+    @XBlock.json_handler
+    def initialize(self, data, suffix=''):
+
+        settings = {
+            "tick_period": self.tick_period,
+        }
+
+        return settings
+
     @staticmethod
     def clear_data(self):
         del self.sessions[:]
@@ -79,24 +93,40 @@ class AnalyticsExtrasXBlock(XBlock):
 
     @staticmethod
     def session_start(self):
-
+        """
+        Start a new student session and record the time when it happens
+        """
         print ("===== Session started at: " + str(datetime.datetime.now()))
+        self.sessions.append([str(datetime.datetime.now()), "", ""])
 
-        self.sessions.append(
-            [str(datetime.datetime.now()), ""]
-        )
+    @XBlock.json_handler
+    def session_tick(self, data, suffix=''):
+        """
+        Record a periodic tick while the student views this XBlock.
+        A safety measure in case their browser or tab crashes.
+        """
+        if len(self.sessions) > 0:
+            print ("===== Session tick at: " + str(datetime.datetime.now()))
+            self.sessions[-1][1] = str(datetime.datetime.now())
+
+        return {}
 
     @XBlock.json_handler
     def session_end(self, data, suffix=''):
+        """
+        End a student session and record the time when it happens
+        """
 
-        content = {"msg": "session_ended"}
+        if len(self.sessions) > 0:
+            print ("===== Session ended at: " + str(datetime.datetime.now()))
+            self.sessions[-1][2] = str(datetime.datetime.now())
 
-        print ("===== Session ended at: " + str(datetime.datetime.now()))
-        self.sessions[-1][1] = str(datetime.datetime.now())
-
-        return content
+        return {}
 
     def student_view(self, context=None):
+        """
+        The LMS view
+        """
 
         fragment = Fragment()
         content = {'self': self}
@@ -105,12 +135,16 @@ class AnalyticsExtrasXBlock(XBlock):
 
         fragment.add_content(render_template('templates/analyticsextras.html', content))
         fragment.add_css(load_resource("static/css/analyticsextras.css"))
-        fragment.add_javascript(load_resource("static/js/analyticsextras.js"))
+        #fragment.add_javascript(load_resource('static/js/analyticsextras.js'))
+        fragment.add_javascript(render_template('static/js/analyticsextras.js', content))
         fragment.initialize_js('AnalyticsExtrasXBlock')
 
         return fragment
 
     def studio_view(self, context=None):
+        """
+        The CMS view
+        """
 
         fragment = Fragment()
         content = {'self': self}
@@ -124,7 +158,6 @@ class AnalyticsExtrasXBlock(XBlock):
 
     @staticmethod
     def workbench_scenarios():
-        """A canned scenario for display in the workbench."""
         return [
             ("AnalyticsExtrasXBlock",
              """<vertical_demo>
--- analyticsextras/static/js/analyticsextras.js
+++ analyticsextras/static/js/analyticsextras.js
@@ -1,17 +1,39 @@
 function AnalyticsExtrasXBlock(runtime, xblock_element) {
 
-    $('.button-previous').hide();
-    $('.button-next').hide();
-    $('.sequence-bottom').hide();
+    var sequence_list = [];
+
+    /*
+    ajax request here to change the settings above
+     */
+
+
+    $('.button-previous').toggle("{{ self.hide_nav_buttons }}".toLocaleLowerCase());
+    $('.button-next').toggle("{{ self.hide_nav_buttons }}".toLocaleLowerCase());
+    $('.sequence-bottom').toggle("{{ self.hide_sequence_bottom }}".toLocaleLowerCase());
     $("#sequence-list li").each(function() {
         $(this).hide();
     });
 
+    // Send the server a ping
+    function ticks(freq) {
+
+        setInterval(function() {
+            $.ajax({
+                type: "POST",
+                url: runtime.handlerUrl(xblock_element, 'session_tick'),
+                data: JSON.stringify({}),
+                async: false
+            });
+        }, freq);
+
+    }
+
     $(function ($) {
-
-
+        ticks("{{ self.tick_period }}");
+        console.log("test");
     });
 
+    // Tell the server that this session is over
     $( window ).unload(function() {
         $.ajax({
             type: "POST",
